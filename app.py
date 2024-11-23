e893c8eee2926958412003e2b492f867d16fb194
ideepak9898 <deepakraj33333@gmail.com>
2024-11-23 16:22:03 +0530

    Deepak Kumar Raj

---
+++ app.py
@@ -0,0 +1,37 @@
+import pickle
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    
+    data_array = np.array(list(data.values())).reshape(1, -1)
+    
+    output = model.predict(data_array)
+    # print(output[0])
+    
+    return jsonify(output[0])
+
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x)for x in request.form.values()]
+    final_input = data = np.array(data).reshape(1,-1)
+
+    output = model.predict(final_input)[0]
+    
+    return render_template("home.html",prediction_text = "The house prediction price is ₹ {}".format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
